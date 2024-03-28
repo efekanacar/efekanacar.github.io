b61504778d425535aeedb5b9278cc9736c0970ff
Efekan ACAR <efekan48acar@gmail.com>
2024-03-28 04:42:32 +0300

    1

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,18 +5,37 @@ const nav = document.querySelector('.mobile-nav');
 const navMenuBtn = document.querySelector('.nav-menu-btn');
 const navCloseBtn = document.querySelector('.nav-close-btn');
 
-// LinkedIn Button
-const linkedinBtn = document.querySelector('.linkedin-btn');
 
 // navToggle function
-const navToggleFunc = function () {
-    nav.classList.toggle('active');
-}
+const navToggleFunc = function () { nav.classList.toggle('active'); }
 
 navMenuBtn.addEventListener('click', navToggleFunc);
 navCloseBtn.addEventListener('click', navToggleFunc);
 
-// LinkedIn Button Click Event
-linkedinBtn.addEventListener('click', function() {
-    window.open('https://www.linkedin.com/in/efekan-acar-1218911b4/', '_blank');
-});
+
+
+// theme toggle variables
+const themeBtn = document.querySelectorAll('.theme-btn');
+
+
+for (let i = 0; i < themeBtn.length; i++) {
+
+  themeBtn[i].addEventListener('click', function () {
+
+    // toggle `light-theme` & `dark-theme` class from `body`
+    // when clicked `theme-btn`
+    document.body.classList.toggle('light-theme');
+    document.body.classList.toggle('dark-theme');
+
+    for (let i = 0; i < themeBtn.length; i++) {
+
+      // When the `theme-btn` is clicked,
+      // it toggles classes between `light` & `dark` for all `theme-btn`.
+      themeBtn[i].classList.toggle('light');
+      themeBtn[i].classList.toggle('dark');
+
+    }
+
+  })
+
+}
