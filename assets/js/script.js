ae02304a6d97dd01dd3d9c5fd248973a3a2a080f
Efekan ACAR <efekan48acar@gmail.com>
2024-03-28 04:11:06 +0300

    1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,59 +1,41 @@
 'use strict';
 
+// navbar variables
+const nav = document.querySelector('.mobile-nav');
+const navMenuBtn = document.querySelector('.nav-menu-btn');
+const navCloseBtn = document.querySelector('.nav-close-btn');
 
 
-/**
- * add event on element
- */
+// navToggle function
+const navToggleFunc = function () { nav.classList.toggle('active'); }
 
-const addEventOnElem = function (elem, type, callback) {
-  if (elem.length > 1) {
-    for (let i = 0; i < elem.length; i++) {
-      elem[i].addEventListener(type, callback);
+navMenuBtn.addEventListener('click', navToggleFunc);
+navCloseBtn.addEventListener('click', navToggleFunc);
+
+
+
+// theme toggle variables
+const themeBtn = document.querySelectorAll('.theme-btn');
+
+
+for (let i = 0; i < themeBtn.length; i++) {
+
+  themeBtn[i].addEventListener('click', function () {
+
+    // toggle `light-theme` & `dark-theme` class from `body`
+    // when clicked `theme-btn`
+    document.body.classList.toggle('light-theme');
+    document.body.classList.toggle('dark-theme');
+
+    for (let i = 0; i < themeBtn.length; i++) {
+
+      // When the `theme-btn` is clicked,
+      // it toggles classes between `light` & `dark` for all `theme-btn`.
+      themeBtn[i].classList.toggle('light');
+      themeBtn[i].classList.toggle('dark');
+
     }
-  } else {
-    elem.addEventListener(type, callback);
-  }
+
+  })
+
 }
-
-
-
-/**
- * navbar toggle
- */
-
-const navbar = document.querySelector("[data-navbar]");
-const navbarLinks = document.querySelectorAll("[data-nav-link]");
-const navToggler = document.querySelector("[data-nav-toggler]");
-
-const toggleNavbar = function () {
-  navbar.classList.toggle("active");
-  this.classList.toggle("active");
-}
-
-addEventOnElem(navToggler, "click", toggleNavbar);
-
-const closeNavbar = function () {
-  navbar.classList.remove("active");
-  navToggler.classList.remove("active");
-}
-
-addEventOnElem(navbarLinks, "click", closeNavbar);
-
-
-
-/**
- * search bar toggle
- */
-
-const searchBar = document.querySelector("[data-search-bar]");
-const searchTogglers = document.querySelectorAll("[data-search-toggler]");
-const overlay = document.querySelector("[data-overlay]");
-
-const toggleSearchBar = function () {
-  searchBar.classList.toggle("active");
-  overlay.classList.toggle("active");
-  document.body.classList.toggle("active");
-}
-
-addEventOnElem(searchTogglers, "click", toggleSearchBar);
